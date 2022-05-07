57b283836723541a23f48058ed333440f1cdea1a
huanY <3010953083@qq.com>
2022-05-07 15:59:08 +0800

    项目整体完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,45 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-import Home from '../components/Home.vue'
-import Welcome from '../components/Welcome.vue'
-import User from '../components/user/Users.vue'
-import Roles from '../components/role/Roles.vue'
-import Rights from '../components/role/Rights.vue'
-import GoodsList from '../components/GoodsManagement/GoodsList.vue'
-import Add from '../components/GoodsManagement/Add.vue'
-import UpdateGoods from '../components/GoodsManagement/UpdateGoods.vue'
-import Params from '../components/GoodsManagement/Params.vue'
-import GoodsClassify from '../components/GoodsManagement/GoodsClassify.vue'
-import Orders from '../components/Orders/order.vue'
-import Reports from '../components/Reports/reports.vue'
+// 路由懒加载
+// import Login from '../components/Login.vue'
+const Login = () =>
+    import ( /* webpackChunkName:'Login_Home_Welcome' */ '../components/Login.vue')
+    // import Home from '../components/Home.vue'
+const Home = () =>
+    import ( /* webpackChunkName:'Login_Home_Welcome' */ '../components/Home.vue')
+    // import Welcome from '../components/Welcome.vue'
+const Welcome = () =>
+    import ( /* webpackChunkName:'Login_Home_Welcome' */ '../components/Welcome.vue')
+    // import User from '../components/user/Users.vue'
+const User = () =>
+    import ( /* webpackChunkName:'Login_Home_GoodsList' */ '../components/user/Users.vue')
+    // import Roles from '../components/role/Roles.vue'
+const Roles = () =>
+    import ( /* webpackChunkName:'Login_Home_GoodsList' */ '../components/role/Roles.vue')
+    // import Rights from '../components/role/Rights.vue'
+const Rights = () =>
+    import ( /* webpackChunkName:'Login_Home_GoodsList' */ '../components/role/Rights.vue')
+    // import GoodsList from '../components/GoodsManagement/GoodsList.vue'
+const GoodsList = () =>
+    import ( /* webpackChunkName:'Login_Home_GoodsList' */ '../components/GoodsManagement/GoodsList.vue')
+    // import Add from '../components/GoodsManagement/Add.vue'
+const Add = () =>
+    import ( /* webpackChunkName:'Add_UpdateGoods_Params' */ '../components/GoodsManagement/Add.vue')
+    // import UpdateGoods from '../components/GoodsManagement/UpdateGoods.vue'
+const UpdateGoods = () =>
+    import ( /* webpackChunkName:'Add_UpdateGoods_Params' */ '../components/GoodsManagement/UpdateGoods.vue')
+    // import Params from '../components/GoodsManagement/Params.vue'
+const Params = () =>
+    import ( /* webpackChunkName:'Add_UpdateGoods_Params' */ '../components/GoodsManagement/Params.vue')
+    // import GoodsClassify from '../components/GoodsManagement/GoodsClassify.vue'
+const GoodsClassify = () =>
+    import ( /* webpackChunkName:'GoodsClassify_Orders_Reports' */ '../components/GoodsManagement/GoodsClassify.vue')
+    // import Orders from '../components/Orders/order.vue'
+const Orders = () =>
+    import ( /* webpackChunkName:'GoodsClassify_Orders_Reports' */ '../components/Orders/order.vue')
+    // import Reports from '../components/Reports/reports.vue'
+const Reports = () =>
+    import ( /* webpackChunkName:'GoodsClassify_Orders_Reports' */ '../components/Reports/reports.vue')
 
 Vue.use(VueRouter)
 
